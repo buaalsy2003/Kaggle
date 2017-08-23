6d12b77970442a2bcac9b33e4cb7eaef5942c7d7
Shiyong Lin <buaalsy2002@gmail.com>
2017-08-22 20:58:32 -0400

    Update InvasiveCode.py

--- InvasiveSpeciesMonitoring/InvasiveCode.py
+++ InvasiveSpeciesMonitoring/InvasiveCode.py
@@ -1,12 +1,3 @@
-# kaggle kernel of AlGiLa
-# I need to thanks the following contributors inspiring me on this kernel
-
-# the function implementing k-fold come from the kernel provided by Finlay Liu 
-# The idea to use Inception v3 come from the kernel provided by Ogurtsov it was in R, I wrote it in python
-
-# This Python 3 environment comes with many helpful analytics libraries installed
-# It is defined by the kaggle/python docker image: https://github.com/kaggle/docker-python
-# For example, here's several helpful packages to load in 
 
 import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
